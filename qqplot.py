db6b5361ed421d841eb0532ba42f60e1f0bda513
Zilong-Li <zimusen94@gmail.com>
2021-12-14 12:30:59 +0100

    update qqplot

--- qqplot.py
+++ qqplot.py
@@ -16,7 +16,7 @@ def _plot(x, y, ax=None, color=None, ablinecolor="r", alpha=0.8, **kwargs):
 
     ax : matplotlib axis, optional
 
-    color : the dots color in the plot, optional 
+    color : the dots color in the plot, optional
 
     kwargs : key, value pairings passed to ``plt.scatter()``
 
@@ -30,18 +30,22 @@ def _plot(x, y, ax=None, color=None, ablinecolor="r", alpha=0.8, **kwargs):
 
     # Get the color from the current color cycle
     if color is None:
-        line, = ax.plot(0, x.mean())
+        (line,) = ax.plot(0, x.mean())
         color = line.get_color()
         line.remove()
 
-    ax.scatter(x, y, c=color, alpha=alpha, edgecolors='none', **kwargs)
+    ax.scatter(x, y, c=color, alpha=alpha, edgecolors="none", **kwargs)
     ax.set_xlim(xmin=x.min(), xmax=1.05 * x.max())
     ax.set_ylim(ymin=y.min())
 
     if ablinecolor:
         # plot the y=x line
-        ax.plot([x.min(), ax.get_xlim()[1]], [x.min(), ax.get_xlim()[1]],
-                color=ablinecolor, linestyle="-")
+        ax.plot(
+            [x.min(), ax.get_xlim()[1]],
+            [x.min(), ax.get_xlim()[1]],
+            color=ablinecolor,
+            linestyle="-",
+        )
 
     return ax
 
@@ -56,14 +60,18 @@ def _group_bins(cutoff, bins):
     sigp = []
     N = 0
     c = -np.log10(cutoff)
-    sys.stdout.write(strftime("%a, %d %b %Y %H:%M:%S", localtime())
-                     + f" : start to process the input!\n")
+    sys.stdout.write(
+        strftime("%a, %d %b %Y %H:%M:%S", localtime())
+        + f" : start to process the input!\n"
+    )
     try:
         for line in sys.stdin:
             N += 1
             if N % 100000000 == 0:
-                sys.stdout.write(strftime("%a, %d %b %Y %H:%M:%S", localtime())
-                                 + f" : reaching the {N}-th line.\n")
+                sys.stdout.write(
+                    strftime("%a, %d %b %Y %H:%M:%S", localtime())
+                    + f" : reaching the {N}-th line.\n"
+                )
             p = -np.log10(float(line))
             if p > c:
                 sigp.append(p)
@@ -76,8 +84,10 @@ def _group_bins(cutoff, bins):
     except KeyboardInterrupt:
         pass
 
-    sys.stdout.write(strftime("%a, %d %b %Y %H:%M:%S", localtime())
-                     + f" : reached the end! the total number of lines is {N}!\n")
+    sys.stdout.write(
+        strftime("%a, %d %b %Y %H:%M:%S", localtime())
+        + f" : reached the end! the total number of lines is {N}!\n"
+    )
     size = len(sigp)
     obs = sorted(sigp, reverse=True)
     exp = [-np.log10((i + 1 - 0.5) / N) for i in np.arange(size)]
@@ -93,9 +103,22 @@ def _group_bins(cutoff, bins):
     return np.array(obs), np.array(exp)
 
 
-def qqplot(figname=None, x=None, y=None, cutoff=1e-4, bins=1000, ax=None,
-           title=None, color=None, alpha=0.8, ablinecolor='r',
-           dpi=300, xlabel=None, ylabel=None, **kwargs):
+def qq(
+    x=None,
+    y=None,
+    figname=None,
+    cutoff=1e-4,
+    bins=1000,
+    ax=None,
+    title=None,
+    color=None,
+    alpha=0.8,
+    ablinecolor="r",
+    dpi=300,
+    xlabel=None,
+    ylabel=None,
+    **kwargs,
+):
 
     if x is None:
         # read data from stdin
@@ -109,8 +132,7 @@ def qqplot(figname=None, x=None, y=None, cutoff=1e-4, bins=1000, ax=None,
         else:
             o = x
             e = y
-    ax = _plot(e, o, ax=ax, color=color, ablinecolor=ablinecolor,
-               alpha=alpha, **kwargs)
+    ax = _plot(e, o, ax=ax, color=color, ablinecolor=ablinecolor, alpha=alpha, **kwargs)
     if xlabel is None:
         xlabel = r"Expected $-log_{10}{(P)}$"
     if ylabel is None:
@@ -128,27 +150,56 @@ def qqplot(figname=None, x=None, y=None, cutoff=1e-4, bins=1000, ax=None,
 def main():
 
     parser = argparse.ArgumentParser(
-        description="QQ plot on the fly! only read data from the pipe!")
-    parser.add_argument("-cutoff", metavar="FLOAT",
-                        nargs='?', default=1e-4, type=float,
-                        help="pval bigger than this cutoff will be grouped into bins.[1e-4]")
-    parser.add_argument("-bins", metavar="INT",
-                        nargs='?', default=1000, type=int,
-                        help="the number of bins to use.[1000]")
-    parser.add_argument("-out", metavar="FILE",
-                        help="prefix of output files")
-    parser.add_argument("-title", metavar="STRING",
-                        help="title of the plot")
+        description="QQ plot on the fly! only read data from the pipe!"
+    )
+    parser.add_argument(
+        "-cutoff",
+        metavar="FLOAT",
+        nargs="?",
+        default=1e-4,
+        type=float,
+        help="pval bigger than this cutoff will be grouped into bins. [%(default)s]",
+    )
+    parser.add_argument(
+        "-bins",
+        metavar="INT",
+        nargs="?",
+        default=1000,
+        type=int,
+        help="the number of bins to use. [%(default)s]",
+    )
+    parser.add_argument(
+        "-csv",
+        default=False,
+        action="store_true",
+        dest="csv",
+        help="read csv (two columns) from stdin. [%(default)s]",
+    )
+    parser.add_argument("-out", metavar="FILE", help="prefix of output files")
+    parser.add_argument("-title", metavar="STRING", help="title of the plot")
     args = parser.parse_args()
 
     assert args.out is not None, "please specify the file of output."
 
     f, ax = plt.subplots(figsize=(6, 6), facecolor="w", edgecolor="k")
-    qqplot(figname=args.out + ".png",
-           cutoff=args.cutoff,
-           bins=args.bins,
-           title=args.title,
-           ax=ax)
+    if args.csv:
+        import pandas as pd
+
+        d = pd.read_csv(sys.stdin, header=0, names=["o", "e"])
+        qq(
+            x=d["o"].values,
+            y=d["e"].values,
+            figname=args.out + ".png",
+            ax=ax,
+        )
+    else:
+        qq(
+            figname=args.out + ".png",
+            cutoff=args.cutoff,
+            bins=args.bins,
+            title=args.title,
+            ax=ax,
+        )
 
 
 if __name__ == "__main__":
